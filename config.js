1c0ac93fa5fc7e7602abed1d453041f787bdb98d
Ashok78910 <ak0671527@gmail.com>
2020-10-08 18:21:04 +0530

    oauth

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl' : 'mongodb://localhost:27017/conFusion'
+    'mongoUrl' : 'mongodb://localhost:27017/conFusion',
+    'facebook': {
+        clientId: '2854798818071173',
+        clientSecret: 'eef86749903da3dbab64e4ae4ecadc87'
+}
 }
